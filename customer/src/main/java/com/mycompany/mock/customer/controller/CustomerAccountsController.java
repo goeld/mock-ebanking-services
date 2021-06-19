a26fc627462ce1cce14b14d75a66ee6c5155084d
deepak <admin@Admins-MacBook-Pro.local>
2021-06-19 08:40:40 +0800

    Fixes done as per Customer Account Txn Client requests

--- customer/src/main/java/com/mycompany/mock/customer/controller/CustomerAccountsController.java
+++ customer/src/main/java/com/mycompany/mock/customer/controller/CustomerAccountsController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class CustomerAccountsController {
 
@@ -15,6 +17,11 @@ public class CustomerAccountsController {
     @GetMapping("/customer/account/{customerId}/{currency}")
     public AccountDetails getCustomerAccount(@PathVariable String customerId, @PathVariable String currency){
         return customerAccountService.getCustomerAccountDetails(customerId, currency);
+    }
+
+    @GetMapping("/customer/account/{customerId}")
+    public List<AccountDetails> getCustomerAccount(@PathVariable String customerId){
+        return customerAccountService.getCustomerAccountDetails(customerId);
     }
 
 }
--- customer/src/main/java/com/mycompany/mock/customer/service/CustomerAccountService.java
+++ customer/src/main/java/com/mycompany/mock/customer/service/CustomerAccountService.java
@@ -3,7 +3,6 @@ package com.mycompany.mock.customer.service;
 import com.mycompany.mock.customer.entity.AccountDetails;
 import org.springframework.stereotype.Service;
 
-import java.nio.channels.AcceptPendingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,29 +14,35 @@ public class CustomerAccountService {
 
     private static Map<String, List<AccountDetails>> customerAccountMap;
 
+    static {
+        initialiseCustomerAccointMap();
+    }
+
+    public static void initialiseCustomerAccointMap(){
+        customerAccountMap = new HashMap<>();
+        String customer1 = "P-0123456789";
+        List<AccountDetails> cust1AccountList = new ArrayList<>();
+        cust1AccountList.add(new AccountDetails("CH93-0000-0000-93454", customer1, "INR"));
+        cust1AccountList.add(new AccountDetails("CH93-0000-0000-93451", customer1, "SGD"));
+        cust1AccountList.add(new AccountDetails("CH93-0000-0000-93452", customer1, "SGD"));
+        customerAccountMap.put(customer1, cust1AccountList);
+
+
+        String customer2 = "P-0123456790";
+        List<AccountDetails> cust2AccountList = new ArrayList<>();
+        cust2AccountList.add(new AccountDetails("CH93-0000-0000-93459", "", "USD"));
+        cust2AccountList.add(new AccountDetails("CH93-0000-0000-93458", "", "USD"));
+        customerAccountMap.put(customer2, cust2AccountList);
+
+        String customer3 = "P-0123456791";
+        List<AccountDetails> cust3AccountList = new ArrayList<>();
+        cust3AccountList.add(new AccountDetails("CH93-0000-0000-93457", "", "USD"));
+        customerAccountMap.put(customer3, cust3AccountList);
+    }
+
     public AccountDetails getCustomerAccountDetails(String customerId, String currency) {
 
-        if (customerAccountMap == null) {
-            customerAccountMap = new HashMap<>();
-            String customer1 = "P-0123456789";
-            List<AccountDetails> cust1AccountList = new ArrayList<>();
-            cust1AccountList.add(new AccountDetails("CH93-0000-0000-93454", customer1, "INR"));
-            cust1AccountList.add(new AccountDetails("CH93-0000-0000-93451", customer1, "SGD"));
-            cust1AccountList.add(new AccountDetails("CH93-0000-0000-93452", customer1, "SGD"));
-            customerAccountMap.put(customer1, cust1AccountList);
-
-
-            String customer2 = "P-0123456790";
-            List<AccountDetails> cust2AccountList = new ArrayList<>();
-            cust2AccountList.add(new AccountDetails("CH93-0000-0000-93459", "", "USD"));
-            cust2AccountList.add(new AccountDetails("CH93-0000-0000-93458", "", "USD"));
-            customerAccountMap.put(customer2, cust2AccountList);
-
-            String customer3 = "P-0123456791";
-            List<AccountDetails> cust3AccountList = new ArrayList<>();
-            cust3AccountList.add(new AccountDetails("CH93-0000-0000-93457", "", "USD"));
-            customerAccountMap.put(customer3, cust3AccountList);
-        }
+        List<AccountDetails> accountDetails = this.getCustomerAccountDetails(customerId);
 
         return
                 customerAccountMap.get(customerId).stream()
@@ -46,6 +51,15 @@ public class CustomerAccountService {
                             return ism;
                         })
                         .collect(Collectors.toList()).get(0);
+    }
+
+    public List<AccountDetails> getCustomerAccountDetails(String customerId) {
+
+        if (customerAccountMap == null) {
+            initialiseCustomerAccointMap();
+        }
+
+        return customerAccountMap.get(customerId);
     }
 
 
